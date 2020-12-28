dea796d1c3ee6be07038d4dae8d09b36b99ef8f8
pratixashah <shah.pratixa@gmail.com>
2020-12-27 23:24:17 -0800

    improved for performance

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,75 +1,62 @@
 import os
 import csv
 
-# To get file path
-file_path = os.path.join(".", "Resources","election_data.csv")
+# To get data file
+load_file = os.path.join(".", "Resources","election_data.csv")
+
+# To set the path for new output file
+output_file = os.path.join(".", "Analysis", "PyPoll_Analysis.txt")
+
+# reset variable to 0
+vote_count = 0
+
+# To store Candidate list with their vote count resp. in dictionary
+dict_candidate_list ={}
 
 # To open file to read
-with open(file_path) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
+with open(load_file) as data_file:
+    csvreader = csv.reader(data_file, delimiter=',')
     
     # To get Header
-    csvheader = next(csvreader)
+    header = next(csvreader)
     
-    # reset variables to 0
-    vote_count = 0
-    
-    def_candidate_list ={}
-    
-#     candidate_list = []
-#     candidate_count_list = []
-
-    # To get first row
-    first_row = next(csvreader)
-    
-    # To get count from first row
-    vote_count = 1
-    
-    # To get candidate Name from first row and set its count to 1
-    def_candidate_list[first_row[2]] = 1
-
-    # To read a file from next row 
+    # To read a file
     for row in csvreader:
         
         # To get Total vote count
         vote_count += 1
         
-        # To get all Candidates and its vote count resp.
-        if(row[2] in def_candidate_list):
-            def_candidate_list[row[2]] += 1
-        else:
-            def_candidate_list[row[2]] = 1
+        # If candidate is not in dictionary then add candidate to it                
+        if(row[2] not in dict_candidate_list):
+            dict_candidate_list[row[2]] = 0
+        
+        # To change vote count to +1 for the same candidate resp. in dictionary
+        dict_candidate_list[row[2]] += 1
 
-# To print Analysis
+# To get Winner who has max no. of votes
+max_key = max(dict_candidate_list, key=dict_candidate_list.get)
+
+# To concate output string with all result 
 output = (
-    f"\nElection Analysis"
-    f"\n---------------------------------------------"
-    f"\nTotal Votes: {vote_count}"
-    f"\n---------------------------------------------"
+    "\nElection Results"+
+    "\n----------------------------" +
+    "\nTotal Votes: "+ str(vote_count) +
+    f"\n----------------------------\n"
 )
 
+# To concate all candidates with number of votes and percentage resp.
+for candidate in dict_candidate_list:
+    output += f"{candidate}: {(dict_candidate_list[candidate]/vote_count)*100:.3f}% ({dict_candidate_list[candidate]})\n" 
+
+output += "----------------------------\n"
+
+# To concate Winner
+output += "Winner: " + str(max_key) 
+output += "\n----------------------------"
+
+# To print Analysis
 print(output)
-
-# To print all candidates with total number of votes resp.
-for candidate in def_candidate_list:
-    print(f"{candidate}: {(def_candidate_list[candidate]/vote_count)*100:.3f}% ({def_candidate_list[candidate]})")
-
-print("----------------------------")
-
-# To print Winner who has max no. of votes
-max_key = max(def_candidate_list, key=def_candidate_list.get)
-
-print(f"Winner: {max_key}")
-print("----------------------------")
-
-output_file = os.path.join(".", "Analysis", "PyPoll_Analysis.txt")
 
 with open(output_file, "w", newline='') as analysis_file:
     analysis_file.write(output)
-    for candidate in def_candidate_list:
-        analysis_file.write(f"\n{candidate}: {(def_candidate_list[candidate]/vote_count)*100:.3f}% ({def_candidate_list[candidate]})")
-
-    analysis_file.write("\n----------------------------")
-
-    analysis_file.write(f"\nWinner: {max_key}")
-    analysis_file.write("\n----------------------------")
+    
